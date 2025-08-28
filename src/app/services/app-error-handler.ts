262eff9f8588ebca314851745f54bf1f33d814c5
Westley Tan <westley.tan@ontariomd.com>
2025-08-28 15:12:11 -0400

    Add update label

--- src/app/services/app-error-handler.ts
+++ src/app/services/app-error-handler.ts
@@ -1,12 +1,5 @@
-import {
-  HttpErrorResponse,
-  HttpEvent,
-  HttpHandler,
-  HttpInterceptor,
-  HttpRequest,
-} from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 import { ErrorHandler, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -14,8 +7,6 @@ import { Observable } from 'rxjs';
 export class AppErrorHandler implements ErrorHandler {
   constructor() {}
   handleError(error: HttpErrorResponse): void {
-    if (error.status < 400) return;
-
     if (confirm(`Error: ${error.message}. Reload?`)) location.reload();
   }
 }
